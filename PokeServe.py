db7ea7fa79507e98ad50bcbdfb7fa9761837fa01
Sharkmare <34294231+Sharkmare@users.noreply.github.com>
2023-01-26 15:40:12 +0100

    Add move data to our pool
    Adds move data and validation if a pokemon can learn a move as well as how it can do so

--- PokeServe.py
+++ PokeServe.py
@@ -105,5 +105,35 @@ def get_pokemon_shiny_back(name):
     return jsonify(get_pokemon_data(name, shiny=True, back=True))
 #SPRITE CODE END
 
+#MOVE CODE START
+@app.route('/move/<string:move_name>', methods=['GET'])
+def get_move_info(move_name):
+    url = f'https://pokeapi.co/api/v2/move/{move_name}'
+    response = requests.get(url)
+    data = response.json()
+    move = {}
+    move["name"] = data["name"]
+    move["power"] = data["power"]
+    move["type"] = data["type"]["name"]
+    move["accuracy"] = data["accuracy"]
+    move["pp"] = data["pp"]
+    return jsonify(move)
+
+def check_valid_move(pokemon_name, move_name):
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}'
+    response = requests.get(url)
+    data = response.json()
+    moves = data["moves"]
+    for move in moves:
+        if move_name.lower() == move["move"]["name"].lower():
+            learn_method = move["version_group_details"][0]["move_learn_method"]["name"]
+            if learn_method == "level-up":
+                learn_level = move["version_group_details"][0]["level_learned_at"]
+                return jsonify({"valid": True, "learn_method":learn_method, "learn_level": learn_level})
+            else:
+                return jsonify({"valid": True, "learn_method":learn_method})
+    return jsonify({"valid": False})
+#MOVE CODE END
+
 if __name__ == '__main__':
     app.run(debug=True)
